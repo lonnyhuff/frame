a2bb3f281243c7397ed1a894b6b6ca912d2a69b9
Lonny Huff <lhuff@plaid.com>
2025-04-06 11:03:30 -0400

    Adding Loop

--- script.js
+++ script.js
@@ -1,2 +1,20 @@
-const video = document.getElementById('ghostVid');
-video.playbackRate = 0.7;
+const forward = document.getElementById('ghostVidForward');
+const reverse = document.getElementById('ghostVidReverse');
+
+// Start with forward video
+reverse.style.display = 'none';
+forward.play();
+
+forward.addEventListener('ended', () => {
+  forward.style.display = 'none';
+  reverse.style.display = 'block';
+  reverse.currentTime = 0;
+  reverse.play();
+});
+
+reverse.addEventListener('ended', () => {
+  reverse.style.display = 'none';
+  forward.style.display = 'block';
+  forward.currentTime = 0;
+  forward.play();
+});
